7cb13413fb9778068f41459624a96ac4d972313d
younnacho <131655841+younnacho@users.noreply.github.com>
2023-06-10 20:25:45 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		String a = "\\    /\\";
+		String b = " )  ( ')";
+		String c = "(  /  )";
+		String d = " \\(__)|";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}

aeda260de4bdb08db33a33ce3c749b747d2501ef
younnacho <131655841+younnacho@users.noreply.github.com>
2023-06-10 23:17:41 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17948 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		double A = input.nextDouble();
+		double B = input.nextDouble();
+		
+		System.out.println(A/B);
+	}
+}

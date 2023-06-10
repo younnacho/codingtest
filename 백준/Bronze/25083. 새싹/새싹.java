6d4a26e2977e10d8a65c738ecbf77f8b530eb7c7
younnacho <131655841+younnacho@users.noreply.github.com>
2023-06-10 20:27:51 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		String a = "         ,r'\"7";
+		String b = "r`-_   ,'  ,/";
+		String c = " \\. \". L_r'";
+		String d = "   `~\\/";
+		String f = "      |";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(f);
+	}
+}
